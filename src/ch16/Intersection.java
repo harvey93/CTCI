bef16746840f3aa0cc1dd496c9f0574bdbc449bd
Hrant <harvey.mirijanyan@gmail.com>
2017-09-07 13:12:17 -0700

    Add swap

--- src/ch16/Intersection.java
+++ src/ch16/Intersection.java
@@ -2,9 +2,28 @@ package ch16;
 
 public class Intersection {
 	
-	public static boolean isIntersecting(int start1, int end1, int start2, int end2) {
+	public static boolean isIntersecting(Point start1, Point end1, Point start2, Point end2) {
 		boolean res = true;
 		return res;
 	}
+	
+	public static void swap(Point p1, Point p2) {
+		double x = p1.x;
+		double y = p1.y;
+		p1.x = p2.x;
+		p1.y = p2.y;
+		p2.x = x;
+		p2.y = y;
+	}
 
+}
+
+class Point{
+	double x;
+	double y;
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 }
