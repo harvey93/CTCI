4a0b631617ecf289d6a7e86055fc85f034edb13d
Hrant <harvey.mirijanyan@gmail.com>
2017-09-07 01:30:16 -0700

    Add wordFreq and allWords

--- src/ch16/Test.java
+++ src/ch16/Test.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
+		String [] book = {"hello", "this", "is", "a", "book", ".", "I", "am", "a", "person", "."};
+		System.out.println(wordFreq.allWords(book));
 	}
 }
 
--- src/ch16/wordFreq.java
+++ src/ch16/wordFreq.java
@@ -1,10 +1,27 @@
 package ch16;
+import java.util.*;
 
 public class wordFreq {
 	
 	public static int freqOne(String[] book, String word) {
-		int res = 0;
-		return res;
+		int count = 0;
+		for(String s : book) {
+			if(s.equals(word)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static HashMap<String, Integer> allWords(String[] book){
+		HashMap<String, Integer> map = new HashMap<>();
+		for(String s : book) {
+			if(!map.containsKey(s)) {
+				map.put(s, 0);
+			}
+			map.put(s, map.get(s) + 1);
+		}
+		return map;
 	}
 
 }
