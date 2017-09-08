f116f6119e1c17cb65765377f33b1f182a01595b
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 14:23:44 -0700

    Add multOpt

--- src/ch16/Operations.java
+++ src/ch16/Operations.java
@@ -12,4 +12,20 @@ public class Operations {
 		
 		return a + multiply(a, b - 1);
 	}
+	
+	public static int multOpt(int a, int b) {
+		if(b == 0) {
+			return 0;
+		}
+		
+		if(b == 1) {
+			return a;
+		}
+		
+		if(b % 2 == 0) {
+			return multOpt(a, b / 2) + multOpt(a, b /2);
+		}else {
+			return multOpt(a, (b - 1) / 2) + multOpt(a, (b - 1)/2) + a;
+		}
+	}
 }
