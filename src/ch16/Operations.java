082c2f2c6f96d477a40df06389f306254ea4f265
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 14:18:49 -0700

    Add mulitply

--- src/ch16/Operations.java
+++ src/ch16/Operations.java
@@ -2,4 +2,14 @@ package ch16;
 
 public class Operations {
 
+	public static int multiply(int a, int b) {
+		if(b == 0) {
+			return 0;
+		}
+		if(b == 1) {
+			return a;
+		}
+		
+		return a + multiply(a, b - 1);
+	}
 }
