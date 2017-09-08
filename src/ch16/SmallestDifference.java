c086c95c0aff8b994e61c5576b4aaa5e8c4d3345
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 13:34:23 -0700

    Add brute force solution

--- src/ch16/SmallestDifference.java
+++ src/ch16/SmallestDifference.java
@@ -2,7 +2,16 @@ package ch16;
 
 public class SmallestDifference {
 
-	public static int getDiff(int [] arr1, int [] arr2) {
-		return 5;
+	public static int getDiffBF(int [] arr1, int [] arr2) {
+		int diff = Integer.MAX_VALUE;
+		for(int a : arr1) {
+			for(int b : arr2) {
+				int currentDiff = Math.abs(a - b);
+				if(currentDiff < diff) {
+					diff = currentDiff;
+				}
+			}
+		}
+		return diff;
 	}
 }
