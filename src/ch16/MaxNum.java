d94d46806f5796a911cd035cdc4230d575298c5e
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 14:16:22 -0700

    Start operation

---
+++ src/ch16/MaxNum.java
@@ -0,0 +1,8 @@
+package ch16;
+
+public class MaxNum {
+
+	public static int calc(int a, int b) {
+		return 0;
+	}
+}
--- src/ch16/Test.java
+++ src/ch16/Test.java
@@ -4,9 +4,7 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
-		int[] arr1 = {1,3,15,11,2};
-		int[] arr2 = {23,127,237,19,8};
-		System.out.println(SmallestDifference.getDiffOptimal(arr1, arr2));
+		System.out.println(Operations.multiply(3, 6));
 	}
 }
 
