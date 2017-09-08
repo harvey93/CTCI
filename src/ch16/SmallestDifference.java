c85355fb444c23e3b7bee9a1c47d461055302c4c
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 13:51:39 -0700

    Add optimal solution

--- src/ch16/SmallestDifference.java
+++ src/ch16/SmallestDifference.java
@@ -1,5 +1,8 @@
 package ch16;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class SmallestDifference {
 
 	public static int getDiffBF(int [] arr1, int [] arr2) {
@@ -10,6 +13,37 @@ public class SmallestDifference {
 				if(currentDiff < diff) {
 					diff = currentDiff;
 				}
+				
+				if(diff == 0) {
+					break;
+				}
+			}
+		}
+		return diff;
+	}
+	
+	public static int getDiffOptimal(int [] arr1, int [] arr2) {
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		int diff = Integer.MAX_VALUE;
+		int i = 0;
+		int j = 0;
+		while(i < arr1.length || j < arr2.length) {
+			int val1 = arr1[i];
+			int val2 = arr2[j];
+			int currentDiff = Math.abs(val1 - val2);
+			if(currentDiff < diff) {
+				diff = currentDiff;
+			}
+			
+			if(val1 > val2) {
+				j++;
+				if(j == arr2.length) break;
+			}else if(val1 < val2) {
+				i++;
+				if(i == arr1.length) break;
+			}else {
+				break;
 			}
 		}
 		return diff;
--- src/ch16/Test.java
+++ src/ch16/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		int[] arr1 = {1,3,15,11,2};
 		int[] arr2 = {23,127,237,19,8};
-		System.out.println(SmallestDifference.getDiffBF(arr1, arr2));
+		System.out.println(SmallestDifference.getDiffOptimal(arr1, arr2));
 	}
 }
 
