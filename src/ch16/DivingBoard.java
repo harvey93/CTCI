c16f597a9422a04fafdc4a16254881f6fcaa9928
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 17:38:47 -0700

    Add diving board

--- src/ch16/DivingBoard.java
+++ src/ch16/DivingBoard.java
@@ -5,7 +5,17 @@ public class DivingBoard {
 
 	public static HashSet<Integer> allLengths(int k, int shorter, int longer){
 		HashSet<Integer> result = new HashSet<>();
-		
+		allLengths(k, 0, shorter, longer, result);
 		return result;
+	}
+	
+	public static void allLengths(int k, int total, int shorter, int longer, HashSet<Integer> result) {
+		if(k == 0) {
+			result.add(total);
+			return;
+		}
+		String key = k + "-" + total;
+		allLengths(k - 1, total + shorter, shorter, longer, result);
+		allLengths(k - 1, total + longer, shorter, longer, result);
 	}
 }
