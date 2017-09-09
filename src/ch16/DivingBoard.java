6482debc8baaa071f4e3611c74b3052a092d4dee
Hrant <harvey.mirijanyan@gmail.com>
2017-09-08 17:40:56 -0700

    Add memoization solution

--- src/ch16/DivingBoard.java
+++ src/ch16/DivingBoard.java
@@ -5,17 +5,24 @@ public class DivingBoard {
 
 	public static HashSet<Integer> allLengths(int k, int shorter, int longer){
 		HashSet<Integer> result = new HashSet<>();
-		allLengths(k, 0, shorter, longer, result);
+		HashSet<String> visited = new HashSet<>();
+		allLengths(k, 0, shorter, longer, result, visited);
+		
 		return result;
 	}
 	
-	public static void allLengths(int k, int total, int shorter, int longer, HashSet<Integer> result) {
+	public static void allLengths(int k, int total, int shorter, int longer, HashSet<Integer> result, HashSet<String> visited) {
 		if(k == 0) {
 			result.add(total);
 			return;
 		}
 		String key = k + "-" + total;
-		allLengths(k - 1, total + shorter, shorter, longer, result);
-		allLengths(k - 1, total + longer, shorter, longer, result);
+		if(visited.contains(key)) {
+			
+			return;
+		}
+		allLengths(k - 1, total + shorter, shorter, longer, result, visited);
+		allLengths(k - 1, total + longer, shorter, longer, result, visited);
+		visited.add(key);
 	}
 }
