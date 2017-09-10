83e571e85393ed61bf1055ceec4fc897111ec255
Hrant <harvey.mirijanyan@gmail.com>
2017-09-09 18:23:49 -0700

    Add Block constructor

---
+++ src/ch16/PatternMatch.java
@@ -0,0 +1,10 @@
+package ch16;
+
+public class PatternMatch {
+
+	
+	public static boolean isMatch(String s1, String s2) {
+		boolean res = false;
+		return res;
+	}
+}
--- src/ch16/Test.java
+++ src/ch16/Test.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
-		int [] arr = {-2, -8, -3, -2, -4, -10, 0};
-		System.out.println(ContiguousSequence.calc(arr));
+		System.out.println(PatternMatch.isMatch("catcatgocatgo", "aabab"));
 	}
 }
 
