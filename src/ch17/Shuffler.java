6b928eb4ccb29a65029ea5818f89d91eec5bb82e
Hrant <harvey.mirijanyan@gmail.com>
2017-09-19 17:20:34 -0700

    Add shuffle

--- src/ch17/Shuffler.java
+++ src/ch17/Shuffler.java
@@ -1,8 +1,22 @@
 package ch17;
+import java.util.concurrent.ThreadLocalRandom;;
+
 
 public class Shuffler {
 
 	public static void shuffle(int [] arr) {
 		
+		for(int i = 0; i < arr.length; i++) {
+			int j = rand(i);
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	
+	public static int rand(int max) {
+		int randomNum = ThreadLocalRandom.current().nextInt(0, max + 1);
+		return randomNum;
 	}
 }
--- src/ch17/Test.java
+++ src/ch17/Test.java
@@ -1,9 +1,13 @@
 package ch17;
 
+import java.util.Arrays;
+
 public class Test {
 	
 	public static void main(String[] args) {
-		
+		int [] arr = {1,2,3,4,5,6};
+		Shuffler.shuffle(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
