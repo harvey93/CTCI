9bfb0cc08af2a7df8f375a1d7f38de361c3cf425
Hrant <harvey.mirijanyan@gmail.com>
2017-09-25 12:28:27 -0700

    Start node

--- src/graphs/Node.java
+++ src/graphs/Node.java
@@ -1,5 +1,6 @@
 package graphs;
 
-public class Node {
+public class Node<T> {
 
+	T data;
 }
---
+++ src/graphs/Test.java
@@ -0,0 +1,8 @@
+package graphs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+}
